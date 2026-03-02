672815244f299c5126bd2beee6a2f43071e54cb0
URLeeo <mammadzadaaslan05@gmail.com>
2026-03-02 22:23:57 +0400

    Completed Lab

--- IntroSpringBoot/src/main/java/org/example/introspringboot/controller/PatientsController.java
+++ IntroSpringBoot/src/main/java/org/example/introspringboot/controller/PatientsController.java
@@ -1,4 +1,65 @@
 package org.example.introspringboot.controller;
 
+import org.example.introspringboot.Data.InMemoryDb;
+import org.example.introspringboot.model.Patient;
+import org.example.introspringboot.model.Status;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
 public class PatientsController {
+
+    private final InMemoryDb db;
+
+    public PatientsController(InMemoryDb db) {
+        this.db = db;
+    }
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return db.getAllPatients().stream().toList();
+    }
+
+    @GetMapping("/{patientId}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable int patientId) {
+        var p = db.getPatientById(patientId);
+        if (p == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(p);
+    }
+
+    @GetMapping("/dob")
+    public List<Patient> getPatientsByDobRange(
+            @RequestParam LocalDate from,
+            @RequestParam LocalDate to
+    ) {
+        return db.getAllPatients().stream()
+                .filter(p -> !p.getDateOfBirth().isBefore(from) && !p.getDateOfBirth().isAfter(to))
+                .toList();
+    }
+
+    @GetMapping("/admitting-doctor/department/{department}")
+    public List<Patient> getPatientsByAdmittingDoctorDepartment(@PathVariable String department) {
+        String dep = department.toLowerCase();
+
+        return db.getAllPatients().stream()
+                .filter(p -> {
+                    var doc = db.getEmployeeById(p.getAdmittedBy());
+                    return doc != null && doc.getDepartment().equalsIgnoreCase(dep);
+                })
+                .toList();
+    }
+
+    @GetMapping("/admitting-doctor/status/{status}")
+    public List<Patient> getPatientsByAdmittingDoctorStatus(@PathVariable Status status) {
+        return db.getAllPatients().stream()
+                .filter(p -> {
+                    var doc = db.getEmployeeById(p.getAdmittedBy());
+                    return doc != null && doc.getStatus() == status;
+                })
+                .toList();
+    }
 }
